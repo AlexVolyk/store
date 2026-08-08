982b6e18245efac789a959c84f98e4d8c9f4b9fb
AlexVolyk <alexvolyka@gmail.com>
2026-08-08 05:00:59 +0300

    feat: rest modals and their types

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,1 +1,7 @@
-export {UserModel} from './user.models.ts';
+export { CartModel } from './cart.models.ts';
+export { CategoryModel } from './category.models.ts';
+export { OrderModel } from './order.models.ts';
+export { ProductModel } from './product.models.ts';
+export { ReviewModel } from './review.models.ts';
+export { UserModel } from './user.models.ts';
+export { WishlistModel } from './wishlist.models.ts';
