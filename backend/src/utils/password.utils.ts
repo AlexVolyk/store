aa2b02cccc87405e22e4c067a5af5cf340356f86
AlexVolyk <alexvolyka@gmail.com>
2026-08-29 05:11:44 +0300

    refactor: salt was changed

--- backend/src/utils/password.utils.ts
+++ backend/src/utils/password.utils.ts
@@ -1,4 +1,4 @@
 import bcrypt from 'bcrypt'
-export const hashPassword = (password: string) => bcrypt.hashSync(password, 7) 
+export const hashPassword = (password: string) => bcrypt.hashSync(password, 10) 
 
 export const comparePasswords = (password: string, loginPassword: string)=> bcrypt.compare(password, loginPassword)
